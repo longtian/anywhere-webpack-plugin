6989381eb29f04244de27c93e05b721a5ed30654
龙天 <wyvernnot@gmail.com>
2016-05-30 17:43:29 +0800

    Remove url dependency

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
-var url = require('url');
+var path = require('path');
 var scripts = document.getElementsByTagName('script');
 
 // get current script
 var currentScript = document.currentScript || scripts[scripts.length - 1];
 
-// parse it so we can get parent url
-var scriptUrl = url.parse(currentScript.src);
-var staticPath = url.format({
-  protocol: scriptUrl.protocol,
-  host: scriptUrl.host,
-  pathname: url.resolve(scriptUrl.pathname, '../')
-});
-
 // set webpack publicPath in runtime
-__webpack_require__.p = staticPath;
+__webpack_require__.p = path.dirname(currentScript.src) + '/';
